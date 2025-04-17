f191d817928b2c4cf7b604c8553d93e1458f60bd
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-04-17 02:02:19 +0100

    add route for create

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+import Create from './pages/Tournois/Create';
 import { AppContext } from './context/AppContext';
 import { useContext } from 'react';
 import Home from './pages/Home';
@@ -16,8 +17,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="Register" element={user ?  <Home /> : <Register />} />
-            <Route path="Login" element={user ?  <Home /> : <Login />} />
+            <Route path="/register" element={user ?  <Home /> : <Register />} />
+            <Route path="/login" element={user ?  <Home /> : <Login />} />
+            <Route path="/create" element={user ?  <Create /> : <Login />} />
           </Route>
         </Routes>
       </BrowserRouter>
